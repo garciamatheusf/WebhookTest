c2b5420c8365bfac64dbc681239ab21662f452a4
tzingf15 <2$6? 7x^a9|vB)>
2022-06-27 01:58:06 -0300

    ajustes por alteração de versão de dependências

--- src/main/java/jersey/JerseyStart.java
+++ src/main/java/jersey/JerseyStart.java
@@ -2,14 +2,6 @@ package jersey;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
-
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.ext.Provider;
 
 public class JerseyStart {
 
@@ -35,10 +27,7 @@ public class JerseyStart {
 
         server = new Server(port);
         ServletContextHandler context = new ServletContextHandler(server, "/");
-
-        ServletHolder servlet = new ServletHolder(JERSEY_SERVLET_NAME,
-                new ServletContainer(new JerseyConfiguration()));
-        context.addServlet(servlet, "/*");
+        context.addServlet(JERSEY_SERVLET_NAME, "/*");
 
         server.start();
     }
@@ -49,26 +38,4 @@ public class JerseyStart {
         }
     }
 
-    public class JerseyConfiguration extends ResourceConfig {
-
-        JerseyConfiguration() {
-            packages("jersey.resources");
-            register(new CORSFilter());
-        }
-    }
-
-    @Provider
-    public class CORSFilter implements ContainerResponseFilter {
-
-        @Override
-        public void filter(ContainerRequestContext request,
-                           ContainerResponseContext response) {
-            response.getHeaders().add("Access-Control-Allow-Origin", "*");
-            response.getHeaders().add("Access-Control-Allow-Headers",
-                    "origin, content-type, accept, authorization");
-            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-            response.getHeaders().add("Access-Control-Allow-Methods",
-                    "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        }
-    }
 }
